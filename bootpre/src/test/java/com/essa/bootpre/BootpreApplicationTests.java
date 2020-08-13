c51fd6222cf2d1a9fe204aaccaa16b39bfb0542a
samadhi <chenjundb@sina.com>
2020-08-13 17:14:52 +0800

    Add SpringBoot code

---
+++ bootpre/src/test/java/com/essa/bootpre/BootpreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essa.bootpre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootpreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
